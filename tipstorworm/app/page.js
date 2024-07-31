20807bc306b3f8db0a3bb68f37e1e5300361d56f
ItsMe Prince <mohammadbadshah92@gmail.com>
2024-07-31 13:21:01 +0530

    working on homepage

--- tipstorworm/app/page.js
+++ tipstorworm/app/page.js
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
     <>
-      <h1>Home Page</h1>
+      <main>
+        <>
+          <h1>Tipstor Worm</h1>
+        </>
+        <>
+          <button>Website & Tips</button>
+        </>
+      </main>
     </>
   );
 }
