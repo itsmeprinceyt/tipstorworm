e5f33539c1ae24600f542055fce257993b125acc
ItsMe Prince <mohammadbadshah92@gmail.com>
2024-07-31 15:21:29 +0530

    homepage ready

--- tipstorworm/app/page.jsx
+++ tipstorworm/app/page.jsx
@@ -2,22 +2,20 @@ import './globals.css'
 
 export default function Home() {
   return (
-    <div className="bg-slate-600">
-      <main >
-        <div className="bg-pink-500 flex-col ">
+    <div className="h-screen flex flex-col justify-center ">
+      <div className=" flex flex-col items-center justify-center font-semibold p-8">
           <p>Welcome to</p>
-          <h1 className="text-3xl">Tipstor Worm</h1>
+          <h1 className="text-5xl pb-3 font-bold drop-shadow-xl">Tipstor Worm</h1>
           <p>Curated Web Gems and App Finds</p>
         </div>
-        <div>
-          <button className="bg-red-400">Websites</button>
-          <button>Apps</button>
-          <button>Tips & Tricks</button>
-          <button>Other</button>
+        <div className=" p-6 flex gap-3 justify-center font-semibold text-white">
+        <button className="text-yellow-950 bg-yellow-400 hover:bg-black hover:text-yellow-400 py-2 px-4 rounded w-[130px] shadow-blackcustom hover:shadow-yellowcustom">Websites</button>
+        <button className="text-yellow-950 bg-yellow-400 hover:bg-black hover:text-yellow-400 py-2 px-4 rounded w-[130px] shadow-blackcustom hover:shadow-yellowcustom">Application</button>
+        <button className="text-yellow-950 bg-yellow-400 hover:bg-black hover:text-yellow-400 py-2 px-4 rounded w-[130px] shadow-blackcustom hover:shadow-yellowcustom">Tips & Tricks</button>
+        <button className="text-yellow-950 bg-yellow-400 hover:bg-black hover:text-yellow-400 py-2 px-4 rounded w-[130px] shadow-blackcustom hover:shadow-yellowcustom">Extras</button>
         </div>
-      </main>
-      <footer>
-        <p>Made by @itsmeprinceyt</p>
+        <footer className="fixed bottom-1 left-1/2 transform -translate-x-1/2">
+        <p className="text-black ">Made by @itsmeprinceyt</p>
       </footer>
     </div>
   );
--- tipstorworm/tailwind.config.js
+++ tipstorworm/tailwind.config.js
@@ -12,7 +12,36 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      boxShadow: {
+        'blackcustom': '4px 4px 0px rgba(0, 0, 0, 1)',
+        'yellowcustom': '4px 4px 0px rgba(234, 179, 8, 1)',
+      },
+      textShadow: {
+        'default': '2px 2px 4px rgba(0, 0, 0, 0.5)',
+        'md': '3px 3px 6px rgba(0, 0, 0, 0.5)',
+        'lg': '5px 5px 10px rgba(0, 0, 0, 0.5)',
+        'hard': '5px 5px 0px rgba(0, 0, 0, 1)',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.text-shadow': {
+          textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+        },
+        '.text-shadow-md': {
+          textShadow: '3px 3px 6px rgba(0, 0, 0, 0.5)',
+        },
+        '.text-shadow-lg': {
+          textShadow: '5px 5px 10px rgba(0, 0, 0, 0.5)',
+        },
+        '.text-shadow-hard': {
+          textShadow: '5px 5px 0px rgba(0, 0, 0, 1)',
+        },
+      };
+
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    },
+  ],
 };
