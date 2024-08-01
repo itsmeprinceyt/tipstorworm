c8ad4b8d0c268c8ec29b0977dbf0799a77393156
itsmeprinceyt <114930668+itsmeprinceyt@users.noreply.github.com>
2024-08-01 19:30:15 +0530

    .

--- tipstorworm/lib/connectDB.js
+++ tipstorworm/lib/connectDB.js
@@ -1,6 +1,8 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = ;
+import { config } from 'dotenv';
+config();
+const uri = process.env.DATABASE_URL;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
