be3833e1b02acfa5aeb811eed53f00e9ed8d51ee
ItsMe Prince <mohammadbadshah92@gmail.com>
2024-08-02 12:50:18 +0530

    working on frontend

--- tipstorworm/app/page.jsx
+++ tipstorworm/app/page.jsx
@@ -6,18 +6,19 @@ export default function Home() {
     <div className="h-screen flex flex-col justify-center ">
       <header>
         <button className="py-3 px-3 shadow-md rounded-3xl absolute right-2 top-1">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
-</svg>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
+          </svg>
 
         </button>
       </header>
       <div className="flex flex-col items-center justify-center font-semibold p-8">
-          <p>Welcome to</p>
-          <h1 className="text-5xl pb-3 font-bold drop-shadow-xl">Tipstor Worm</h1>
-          <p >Curated Web Gems and App Finds</p>
-        </div>
-        <div className="p-2 flex gap-3 justify-center font-semibold text-white">
+        <p>Welcome to</p>
+        <h1 className="text-5xl pb-3 font-bold drop-shadow-xl">Tipstor Worm</h1>
+        <p >Curated Web Gems and App Finds</p>
+      </div>
+
+      <div className="p-2 font-semibold grid grid-cols-1 gap-3 place-items-center sm:grid-cols-5 ml-auto mr-auto">
         <Link href="/websites">
           <button className="text-yellow-950 bg-yellow-400 hover:bg-black hover:text-yellow-400 py-2 px-4 rounded w-[130px] shadow-blackcustom hover:shadow-yellowcustom">Websites</button>
         </Link>
@@ -30,16 +31,17 @@ export default function Home() {
         <Link href="/extras">
           <button className="text-yellow-950 bg-yellow-400 hover:bg-black hover:text-yellow-400 py-2 px-4 rounded w-[130px] shadow-blackcustom hover:shadow-yellowcustom">Extras</button>
         </Link>
-        
+
         <Link href="/addingdata">
           <button className="text-green-950 bg-green-500 hover:bg-black hover:text-green-400 py-2 px-4 rounded shadow-blackcustom hover:shadow-greencustom">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-          </svg>
-        </button>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            </svg>
+          </button>
         </Link>
-        </div>
-        <footer className="fixed bottom-2 left-1/2 transform -translate-x-1/2 ">
+      </div>
+
+      <footer className="fixed bottom-2 left-1/2 transform -translate-x-1/2 ">
         <p className="text-red-950 ">Made by @itsmeprinceyt </p>
       </footer>
     </div>
--- tipstorworm/app/websites/page.jsx
+++ tipstorworm/app/websites/page.jsx
@@ -1,20 +1,31 @@
-export default function websites(){
+import Image from "next/image";
+export default function websites() {
     // add mongo db code here to fetch all the data.
-    return(
-        <div className="bg-slate-800 h-screen w-screen flex flex-col gap-3">
-            <div className="bg-red-400 w-screen flex justify-between h-24 mx-2 my-2">
-                <div className="bg-yellow-500 w-1/5">
-                    Photo
+    // fetch the data and see what data has what color then add that color in this ButtonStyle then map all the data one by one and make a html file then put that html file in the return
+    const ButtonStyle= `text-yellow-950 bg-yellow-400 hover:bg-black hover:text-yellow-400 py-2 px-4 rounded shadow-blackcustom hover:shadow-yellowcustom`;
+
+    return (
+        <div className="h-screen w-screen flex flex-col gap-3">
+            <div className="flex justify-start h-36 mx-2 my-2 rounded-lg shadow-yellowcustom min-w-[400px] w-3/5">
+                <div className=" w-1/5 flex justify-center items-center overflow-hidden relative rounded-tl-lg rounded-bl-lg">
+                    <Image
+                        src="/cat.jpg"
+                        fill
+                        style={{ objectFit: 'cover' }}
+                        alt="Cat"
+                    />
                 </div>
-                <div className="flex flex-col w-3/5">
-                    <p>Title</p>
-                    <p>Description</p>
+                <div className=" flex flex-col justify-between w-3/5 px-2 py-2">
+                    <div>
+                        <p className="font-semibold text-lg">Title</p>
+                        <p className="text-sm">This is a dummy description of the asset which has been uploaded to this page</p>
+                    </div>
                     <div className="flex justify-end">
-                        <p>Uploaded by @itsmeprinceyt</p>
+                        <p className="text-xs">Uploaded by @itsmeprinceyt</p>
                     </div>
                 </div>
-                <div className="bg-purple-700 w-1/5">
-                    <button className="bg-white rounded-md px-2 py-2">Click Here!</button>
+                <div className=" w-1/5 flex justify-center items-center">
+                    <button className={ButtonStyle}>Click</button>
                 </div>
             </div>
         </div>
