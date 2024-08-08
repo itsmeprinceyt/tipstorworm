98f0b3d79c3b9b288ae77ecbfd4969b1afcd4c20
ItsMe Prince <mohammadbadshah92@gmail.com>
2024-08-08 15:56:30 +0530

    ui form

--- tipstorworm/app/(components)/card.jsx
+++ tipstorworm/app/(components)/card.jsx
@@ -1,44 +1,29 @@
 import Image from "next/image";
-import CardPreview from "/card";
 
 export default async function Card({ url, alt, title, description, credit, color }) {
     const ButtonStyle = 'w-16 text-yellow-950 bg-yellow-400 hover:bg-black hover:text-yellow-400 p-3 text-xs rounded shadow-2xl';
 
     return (
-        <div>
-            <div className="bg-red-400 min-h-[300px] w-[450px] boxxs:w-[550px] shadow-2xl rounded-xl flex ">
-                <div className="rounded-xl w-1/3 flex items-center justify-center">
-                    <Image className="rounded-xl shadow-2xl"
-                        src={url}
-                        height={115}
-                        width={115}
-                        alt={alt}
-                    />
-                </div>
-                <div className=" flex flex-col justify-between w-3/5 p-2">
-                    <div>
-                        <div className="font-semibold text-lg">{title}</div>
-                        <div className="text-sm">{description}</div>
-                    </div>
-                    <div className="flex justify-end text-xs">{credit}</div>
-                </div>
-                <div className="w-1/5 flex flex-col justify-center items-center gap-5 rounded-xl">
-                    <button className={ButtonStyle}>Visit</button>
-                    <button className={ButtonStyle}>Edit</button>
-                    <button className={ButtonStyle}>Delete</button>
-                </div>
-            </div>
-            <div>
-                <CardPreview
-
-                    url="/cat.jpg"
-                    alt="Cat"
-                    title="Title"
-                    description="This is a dummy description of the asset which has been uploaded to this page"
-                    credit="by @itsmeprinceyt"
-                    color='pink'
-
+        <div className="bg-white min-h-[300px] w-[450px] boxxs:w-[550px] shadow-2xl rounded-xl flex ">
+            <div className="rounded-xl w-1/3 flex items-center justify-center">
+                <Image className="rounded-xl shadow-2xl"
+                    src={url}
+                    height={115}
+                    width={115}
+                    alt={alt}
                 />
+            </div>
+            <div className=" flex flex-col justify-between w-3/5 p-2">
+                <div>
+                    <div className="font-semibold text-lg">{title}</div>
+                    <div className="text-sm">{description}</div>
+                </div>
+                <div className="flex justify-end text-xs">{credit}</div>
+            </div>
+            <div className="w-1/5 flex flex-col justify-center items-center gap-5 rounded-xl">
+                <button className={ButtonStyle}>Visit</button>
+                <button className={ButtonStyle}>Edit</button>
+                <button className={ButtonStyle}>Delete</button>
             </div>
         </div>
     );
--- tipstorworm/app/(components)/form.jsx
+++ tipstorworm/app/(components)/form.jsx
@@ -2,7 +2,6 @@
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
-
 const TipstorForm = () => {
   const router = useRouter();
 
@@ -30,7 +29,7 @@ const TipstorForm = () => {
 
 
   const handleSubmit = async (event) => {
-    event.preventDefault();  
+    event.preventDefault();
     const result = await fetch("/api/TipstorData", {
       method: "POST",
       headers: {
@@ -72,116 +71,120 @@ const TipstorForm = () => {
     "yellow", "green", "blue", "red", "pink", "purple", "indigo", "gray", "black", "white"
   ];
   return (
-    <div>
-      <form className="flex flex-col gap-3 w-1/2"
-        method="post"
-        onSubmit={handleSubmit}>
-        <h1>Adding Data</h1>
+    <div className="bg-blue-400 p-2 w-[1200px]">
+      <h1>Adding Data</h1>
+      <div className="bg-yellow-400 w-4/5">
+        <form 
+          method="post"
+          onSubmit={handleSubmit}>
+          <label>Title</label>
+          <input
+            className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            id="title"
+            name="title"
+            type="text"
+            onChange={handleChange}
+            required={true}
+            value={formData.title}
+          />
 
-        <label>Title</label>
-        <input
-          className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-          id="title"
-          name="title"
-          type="text"
-          onChange={handleChange}
-          required={true}
-          value={formData.title}
-        />
+          <label>Description</label>
+          <textarea
+            className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            id="description"
+            name="description"
+            type="text"
+            onChange={handleChange}
+            required={true}
+            value={formData.description}
+            row="5"
+          />
 
-        <label>Description</label>
-        <textarea
-          className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-          id="description"
-          name="description"
-          type="text"
-          onChange={handleChange}
-          required={true}
-          value={formData.description}
-          row="5"
-        />
+          <label className="font-medium">Photo Description</label>
+          <input
+            className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            id="photo_description"
+            name="photo_description"
+            onChange={handleChange}
+            required={true}
+            value={formData.photo_description}
+          />
 
-        <label className="font-medium">Photo Description</label>
-        <input
-          className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-          id="photo_description"
-          name="photo_description"
-          onChange={handleChange}
-          required={true}
-          value={formData.photo_description}
-        />
+          <label className="font-medium">Photo</label>
+          <input
+            className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            id="photo"
+            name="photo"
+            type="file"
+            onChange={handleFileChange}
+            required={true}
+          />
 
-        <label className="font-medium">Photo</label>
-        <input
-          className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-          id="photo"
-          name="photo"
-          type="file"
-          onChange={handleFileChange}
-          required={true}
-        />
+          <label className="font-medium">Hyperlink Description</label>
+          <input
+            className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            id="hyperlink_description"
+            name="hyperlink_description"
+            onChange={handleChange}
+            required={true}
+            value={formData.hyperlink_description}
+          />
+          <label className="font-medium">Hyperlink</label>
+          <input
+            className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            id="hyperlink"
+            name="hyperlink"
+            type="url"
+            onChange={handleChange}
+            required={true}
+            value={formData.hyperlink}
+          />
 
-        <label className="font-medium">Hyperlink Description</label>
-        <input
-          className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-          id="hyperlink_description"
-          name="hyperlink_description"
-          onChange={handleChange}
-          required={true}
-          value={formData.hyperlink_description}
-        />
-        <label className="font-medium">Hyperlink</label>
-        <input
-          className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-          id="hyperlink"
-          name="hyperlink"
-          type="url"
-          onChange={handleChange}
-          required={true}
-          value={formData.hyperlink}
-        />
-
-        <label className="font-medium">Category</label>
-        <select
-          className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-          id="category"
-          name="category"
-          onChange={handleChange}
-          required={true}
-          value={formData.category}
-        >
-          {categories.map((category) => (
-            <option key={category} value={category}>
-              {category}
-            </option>
-          ))}
-        </select>
-
-        <label className="font-medium">Color</label>
-        <select
-          className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-          id="color"
-          name="color"
-          onChange={handleChange}
-          required={true}
-          value={formData.color}
-        >
-          {colors.map((color) => {
-            const capitalizedColor = color.charAt(0).toUpperCase() + color.slice(1);
-            return (
-              <option key={color} value={color}>
-                {capitalizedColor}
+          <label className="font-medium">Category</label>
+          <select
+            className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            id="category"
+            name="category"
+            onChange={handleChange}
+            required={true}
+            value={formData.category}
+          >
+            {categories.map((category) => (
+              <option key={category} value={category}>
+                {category}
               </option>
-            );
-          })}
-        </select>
+            ))}
+          </select>
 
-        <input
-          type="submit"
-          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600"
-          value="Submit"
-        />
-      </form>
+          <label className="font-medium">Color</label>
+          <select
+            className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            id="color"
+            name="color"
+            onChange={handleChange}
+            required={true}
+            value={formData.color}
+          >
+            {colors.map((color) => {
+              const capitalizedColor = color.charAt(0).toUpperCase() + color.slice(1);
+              return (
+                <option key={color} value={color}>
+                  {capitalizedColor}
+                </option>
+              );
+            })}
+          </select>
+
+          <input
+            type="submit"
+            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600"
+            value="Submit"
+          />
+        </form>
+      </div>
+      <div className="bg-red-400">
+        Hello
+      </div>
     </div>
   );
 }
